6c91c7cc2640c0bb616e3b31d7aab200e0850ff7
trnKhanh <tnk29082003@gmail.com>
2023-10-04 20:55:27 +0700

    add submit api

--- submit.py
+++ submit.py
@@ -6,23 +6,22 @@ res = requests.post(url="https://eventretrieval.one/api/v1/login",
                         "password": "AiL2Oaf8"
                     })
 sessionId = res.json()["sessionId"]
-res.close()
+print(sessionId)
 
 _ = 1
 while (True):
-
     print(f"Submission {_}:")
     _ = _ + 1
     videoId = input("Video Id: ")
     frameId = input("Frame Id: ")
-
+    data = {
+        "item": videoId,
+        "frame": frameId,
+        "session": sessionId,
+    }
     res = requests.get(url="https://eventretrieval.one/api/v1/submit",
-                    json={
-                        "item": videoId,
-                        "frame": frameId,
-                        "session": sessionId,
-                    })
+                       params=data)
     print("-"*100)
     print(f"Status code: {res.status_code}")
     print(f"Body: {res.json()}")
-    print("\n" + "="*100)
+    print("\n\n" + "="*100)
